6ab3b5d09645904dcf84e4879acd69506c9e08f4
venuswhispers <venuswhisperer412@gmail.com>
2024-03-25 10:09:11 +0800

    font

--- app/tokenomics/page.tsx
+++ app/tokenomics/page.tsx
@@ -3,56 +3,9 @@ import React from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import ReactApexChart from 'react-apexcharts';
-// const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 import axios from 'axios';
 
-const chart = {
-          
-  series: [44, 55, 41, 17, 15,15,15,20],
-  options: {
-    // chart: {
-    //   events: {
-    //     mounted: (chart: any) => {
-    //       chart.windowResizeHandler();
-    //     }
-    //   }
-    // },
-    plotOptions: {
-      pie: {
-        startAngle: -90,
-        endAngle: 270
-      }
-    },
-    // dataLabels: {
-    //   enabled: false
-    // },
-    fill: {
-      type: 'gradient',
-    },
-    legend: {
-      // formatter: function(val, opts) {
-      //   return val + " - " + opts.w.globals.series[opts.seriesIndex]
-      // }
-      show: false
-    },
-    // title: {
-    //   text: 'Gradient Donut with custom Start-angle',
-    // },
-    responsive: [{
-      breakpoint: 480,
-      options: {
-        chart: {
-          width: '100%'
-        },
-        legend: {
-          position: 'bottom'
-        }
-      }
-    }]
-  },
-
-
-};
+const HoldersChart = dynamic(() => import("@/components/holdersChart"), { ssr: false });
 
 interface IHolder {
   contributor: string,
@@ -88,9 +41,9 @@ const HomePage = () => {
 
   const _renderHolder = ({ num, address, price }: { num: number, address: string, price: string }) => (
     <div key={num} className='bg-white flex mt-3 w-full px-10 py-3 border-2 rounded-full border-black' style={{ boxShadow: '-5px 5px 0px black' }}>
-      <div className='font-[title] w-1/5 text-2xl text-center'>{num}.</div>
-      <div className='font-[title] w-3/5 text-2xl overflow-hidden text-ellipsis'>{address}</div>
-      <div className='font-[title] w-1/5 text-2xl'>{price} ETH</div>
+      <div className='title w-1/5 text-2xl text-center'>{num}.</div>
+      <div className='title w-3/5 text-2xl overflow-hidden text-ellipsis'>{address}</div>
+      <div className='title w-1/5 text-2xl'>{price} ETH</div>
     </div>
   );
 
@@ -99,26 +52,26 @@ const HomePage = () => {
       <section id="home" className='flex flex-col lg:flex-row items-center max-w-[1200px] mx-auto'>
         <img src="/logo-avatar.gif" alt="Logo-avatar" className='lg:w-5/12 w-full' />
         <div className='py-6 -rotate-3'>
-          <p className=' font-[title] text-4xl font-extrabold'>
+          <p className=' title text-4xl font-extrabold'>
             the most based base baser on base
           </p>
           <img src="/logo.webp" alt="Logo" className='w-full' />
         </div>
       </section>
       <section className='relative mt-[70px] text-center w-full bg-[#A6C2FA] flex-cols pt-10'>
-        <h2 className='font-[title] text-[55px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOKENOMICS</h2>
+        <h2 className='title text-[55px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOKENOMICS</h2>
         <img className='w-full !z-[1000]' src='/tokenomics.png'/>
       </section>
 
       <section className='p-10 mt-10 w-full items-center border-2 border-black border-l-[5px] border-b-[5px] grid lg:grid-cols-2 rounded-2xl max-w-[1200px] mx-auto'>
         <div className='relative'>
-          <ReactApexChart options={chart.options} series={[44, 55, 41, 17, 15,15,15,20]} type="pie" />
+          <HoldersChart />
           <img className='absolute w-20 h-20 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' src='/logo.png'/>
         </div>
         <div className='px-5 text-center'>
           <div className='mb-5 text-left'>
-            <h2 className='font-[title] text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOP 10</h2>
-            <h2 className='font-[title] text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>CONTRIBUTORS</h2>
+            <h2 className='title text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOP 10</h2>
+            <h2 className='title text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>CONTRIBUTORS</h2>
           </div>
 
           {
@@ -134,7 +87,7 @@ const HomePage = () => {
 
       <section className='mt-10 w-full bg-[#A6C2FA] flex justify-center py-10'>
         <div className='w-full items-center max-w-[1200px] bg-[#A6C2FA]'>
-          <h2 className='font-[title] text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOP 100 CONTRIBUTORS</h2>
+          <h2 className='title text-[45px] font-bold [text-shadow:-4px_6px_0px_#000000]'>TOP 100 CONTRIBUTORS</h2>
           {
 
             holders.slice(0,100).map((_holder: IHolder, index: number) => _renderHolder({ num: index + 1, address: _holder.contributor, price: '10' }))
